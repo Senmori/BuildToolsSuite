9f2761d558751b9c5bc7715dd027b1b0bb2df256
Senmori <thesenmori@gmail.com>
2020-03-13 19:24:49 -0400

    Let SpigotConfig used defined config writers and parsers instead of pre-determined ones.

--- src/main/java/net/senmori/project/spigot/config/SpigotConfig.java
+++ src/main/java/net/senmori/project/spigot/config/SpigotConfig.java
@@ -1,7 +1,10 @@
 package net.senmori.project.spigot.config;
 
 import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.core.Config;
 import com.electronwill.nightconfig.core.file.FileNotFoundAction;
+import com.electronwill.nightconfig.core.io.ConfigParser;
+import com.electronwill.nightconfig.core.io.ConfigWriter;
 import com.electronwill.nightconfig.core.io.ParsingMode;
 import com.electronwill.nightconfig.toml.TomlFormat;
 import com.electronwill.nightconfig.toml.TomlParser;
@@ -11,14 +14,20 @@ import java.io.File;
 public class SpigotConfig {
 
     private final CommentedConfig config = TomlFormat.newConcurrentConfig();
-    private final TomlWriter writer = new TomlWriter();
-    private final TomlParser parser = new TomlParser();
+    private final ConfigWriter writer;
+    private final ConfigParser<? super CommentedConfig> parser;
 
     private final File configFile;
 
-    public SpigotConfig(File configFile) {
+    public SpigotConfig(File configFile, ConfigParser<? super CommentedConfig> parser, ConfigWriter configWriter) {
         this.configFile = configFile;
         parser.parse(configFile, this.config, ParsingMode.REPLACE, FileNotFoundAction.CREATE_EMPTY);
+        this.writer = configWriter;
+        this.parser = parser;
+    }
+
+    public SpigotConfig(File configFile) {
+        this(configFile, new TomlParser(), new TomlWriter());
     }
 
     public CommentedConfig getConfig() {
